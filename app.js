17429529e3428362b42432d0b7956c889d1cef85
ivhooijdonk <imkevanhooijdonk@gmail.com>
2024-09-25 15:59:19 +0200

    vanalles gedaan

---
+++ app.js
@@ -0,0 +1,8 @@
+    function openCloseMenu() {
+        var x = document.getElementById("navbar_menu");
+        if (x.style.display === "block") {
+            x.style.display = "none";
+        } else {
+            x.style.display = "block";
+        }
+    } 
