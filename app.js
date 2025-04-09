2127408bd7678c722421c6409d864c89a89db52f
Imke van Hooijdonk <imkevanhooijdonk@Imkes-MacBook-Pro.local>
2025-04-09 13:32:28 +0200

    initiaaaaaaal

--- app.js
+++
@@ -1,8 +0,0 @@
-    function openCloseMenu() {
-        var x = document.getElementById("navbar_menu");
-        if (x.style.display === "block") {
-            x.style.display = "none";
-        } else {
-            x.style.display = "block";
-        }
-    } 
