dc2807d873ca54225e3e6a9b7638ddc516a124c2
Imke van Hooijdonk <imkevanhooijdonk@gmail.com>
2025-04-11 15:10:42 +0200

    fotos

--- js/functions.js
+++ js/functions.js
@@ -128,7 +128,7 @@ $("#submit_btn").click(function() {
    $("a.pagescroll").on("click", function (event) {
       event.preventDefault();
       $("html,body").animate({
-         scrollTop: $(this.hash).offset().top
+         scrollTop: $(this.hash).offset().top,
       }, 1200);
    });
     /*hide menu on mobile click*/
