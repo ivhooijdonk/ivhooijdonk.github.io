0a873289b223df6b52ba79a4d5f3606704cba116
Imke van Hooijdonk <imkevanhooijdonk@gmail.com>
2025-04-16 14:03:36 +0200

    Remove email button from functions.js

--- js/functions.js
+++ js/functions.js
@@ -13,61 +13,6 @@ jQuery(function ($) {
    var $root = $("html, body");
    var $this = $(this);
 
-
-   //Contact Us
-$("#submit_btn").click(function() {
-
-    var user_name = $('input[name=first_name]').val() + ' ' + $('input[name=last_name]').val();
-    var user_email = $('input[name=email]').val();
-    var user_phone = $('input[name=phone]').val();
-    var user_message = $('textarea[name=message]').val();
-
-    //simple validation at client's end
-    var post_data, output;
-    var proceed = true;
-    if (user_name == "") {
-        proceed = false;
-    }
-    if (user_email == "") {
-        proceed = false;
-    }
-   // if (user_phone == "") {
-        //proceed = false;
-   // }
-
-    if (user_message == "") {
-        proceed = false;
-    }
-    //everything looks good! proceed...
-    if (proceed) {
-
-        //data to be sent to server
-        post_data = { 'userName': user_name, 'userEmail': user_email, 'userPhone': user_phone, 'userMessage': user_message };
-
-        //Ajax post data to server
-        $.post('contact.php', post_data, function(response) {
-
-            //load json data from server and output message
-            if (response.type == 'error') {
-                output = '<div class="alert-danger" style="padding:10px; margin-bottom:25px;">' + response.text + '</div>';
-            } else {
-                output = '<div class="alert-success" style="padding:10px; margin-bottom:25px;">' + response.text + '</div>';
-
-                //reset values in all input fields
-                $('.getin_form input').val('');
-                $('.getin_form textarea').val('');
-            }
-
-            $("#result").hide().html(output).slideDown();
-        }, 'json');
-
-    }
-    else {
-           output = '<div class="alert-danger" style="padding:10px; margin-bottom:25px;">Please provide the missing fields.</div>';
-           $("#result").hide().html(output).slideDown();
-    }
-
-});
    $(document).on('contextmenu', function() {
      return false;
    });
